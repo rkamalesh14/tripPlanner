b3e80031c6fc89d54eb981833d5980c56079ccb4
siameseguy <psyhakhom@gmail.com>
2018-05-01 16:23:00 -0400

    yeah!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const mapboxgl = require('mapbox-gl')
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2lhbWVzZWd1eSIsImEiOiJjamV6dGJwNmwwZnprMzFvYTVqZXR6eWF3In0.A391dwHbc5ypIjMcA3SV9g'
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.0, 40.705],
+  zoom: 12,
+  style: 'mapbox://styles/mapbox/streets-v10'
+})
